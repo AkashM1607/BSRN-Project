567477519ff1f20fd2ff265264adf1fc70ca8251
AkashM1607 <83012321+AkashM1607@users.noreply.github.com>
2022-06-01 18:12:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import sys
 spielfeldgröße = 0
 
 while not (8 <= spielfeldgröße <= 12):
-    spielfeldgröße = int(input('Bitte geben Sie die gewünschte Spielfeldgröße ein (AxA).'))
-    if 12 < spielfeldgröße or spielfeldgröße < 8:
-        print('Spielfeldgröße nicht nicht akzeptiert.')
-    else:
-        continue
+    try:
+        spielfeldgröße = int(input('Bitte geben Sie die gewünschte Spielfeldgröße ein (AxA).'))
+        if 12 < spielfeldgröße or spielfeldgröße < 8:
+            print('Spielfeldgröße nicht nicht akzeptiert.')
+        else:
+            continue
+    except ValueError:
+        print("Falsche Eingabe, nur Zahlen erlaubt")
+
 
 space=" "
 hit="X"
@@ -43,12 +47,28 @@ def place_ship():
              "Ship3": 3,
              "Ship4": 2}
 
-    for i in range(1):
-        print("Welche Zeile soll das Schlachtschiff gesetzt werden")
-        Zeile = int(input("Zeile")) - 1
-        print("Welche Spalte soll das Schlachtschiff gesetzt werden")
-        Spalte = (input("Spalte"))
+    for j in range(1):
+        while True:
+            zustand = True
+            while zustand:
+                print("Welche Zeile soll das Schlachtschiff gesetzt werden")
+                Zeile = int(input("Zeile")) - 1
+                for i in range(spielfeldgröße):
+                    if Zeile == i:
+                        zustand = False
+                        break
+            zustand = True
+            while zustand:
+                print("Welche Spalte soll das Schlachtschiff gesetzt werden")
+                Spalte = (input("Spalte"))
+                for i in range(spielfeldgröße):
+                    if Spalte == chr(i + 65):
+                        zustand = False
+                        break
+
+            break
         ShipPosition = int(input("0 für Horizontal und 1 für Vertikal"))
+
 
         for i in range(ships["Ship1"]):
             if ShipPosition == 0:
@@ -56,11 +76,26 @@ def place_ship():
             else:
                 board[i + Zeile][ord(Spalte) - 65] = "x"
 
-    for i in range(2):
-        print("Welche Zeile soll der",i+1,"te Kreuzer gesetzt werden")
-        Zeile = int(input("Zeile")) - 1
-        print("Welche Spalte soll der",i+1,"te Kreuzer gesetzt werden")
-        Spalte = (input("Spalte"))
+    for j in range(2):
+        while True:
+            zustand = True
+            while zustand:
+                print("Welche Zeile soll der",j+1,"te Kreuzer gesetzt werden")
+                Zeile = int(input("Zeile")) - 1
+                for i in range(spielfeldgröße):
+                    if Zeile == i:
+                        zustand = False
+                        break
+            zustand = True
+            while zustand:
+                print("Welche Spalte soll der",j+1,"te Kreuzer gesetzt werden")
+                Spalte = (input("Spalte"))
+                for i in range(spielfeldgröße):
+                    if Spalte == chr(i + 65):
+                        zustand = False
+                        break
+
+            break
         ShipPosition = int(input("0 für Horizontal und 1 für Vertikal"))
         for i in range(ships["Ship2"]):
             if ShipPosition == 0:
@@ -68,11 +103,26 @@ def place_ship():
             else:
                 board[i + Zeile][ord(Spalte) - 65] = "x"
 
-    for i in range(3):
-        print("Welche Zeile soll der",i+1,"te Zerstörer gesetzt werden")
-        Zeile = int(input("Zeile")) - 1
-        print("Welche Spalte soll der",i+1,"te Zerstörer gesetzt werden")
-        Spalte = (input("Spalte"))
+    for j in range(3):
+        while True:
+            zustand = True
+            while zustand:
+                print("Welche Zeile soll der",j+1,"te Zerstörer gesetzt werden")
+                Zeile = int(input("Zeile")) - 1
+                for i in range(spielfeldgröße):
+                    if Zeile == i:
+                        zustand = False
+                        break
+            zustand = True
+            while zustand:
+                print("Welche Spalte soll der",j+1,"te Zerstörer gesetzt werden")
+                Spalte = (input("Spalte"))
+                for i in range(spielfeldgröße):
+                    if Spalte == chr(i + 65):
+                        zustand = False
+                        break
+
+            break
         ShipPosition = int(input("0 für Horizontal und 1 für Vertikal"))
         for i in range(ships["Ship3"]):
             if ShipPosition == 0:
@@ -80,11 +130,26 @@ def place_ship():
             else:
                 board[i + Zeile][ord(Spalte) - 65] = "x"
 
-    for i in range(4):
-        print("Welche Zeile soll das",i+1,"te Uboot gesetzt werden")
-        Zeile = int(input("Zeile")) - 1
-        print("Welche Spalte soll das",i+1,"te Uboot gesetzt werden")
-        Spalte = (input("Spalte"))
+    for j in range(4):
+        while True:
+            zustand = True
+            while zustand:
+                print("Welche Zeile soll das",j+1,"te Uboot gesetzt werden")
+                Zeile = int(input("Zeile")) - 1
+                for i in range(spielfeldgröße):
+                    if Zeile == i:
+                        zustand = False
+                        break
+            zustand = True
+            while zustand:
+                print("Welche Spalte soll das",j+1,"te Uboot gesetzt werden")
+                Spalte = (input("Spalte"))
+                for i in range(spielfeldgröße):
+                    if Spalte == chr(i + 65):
+                        zustand = False
+                        break
+
+            break
         ShipPosition = int(input("0 für Horizontal und 1 für Vertikal"))
         for i in range(ships["Ship4"]):
             if ShipPosition == 0:
@@ -93,13 +158,13 @@ def place_ship():
                 board[i + Zeile][ord(Spalte) - 65] = "x"
 
 place_ship()
-print_grid()
-Zeile=int(input("Welche Zeile"))-1
-Spalte=(input("Welche Spalte"))
-
-
-print_grid()
-if board[Zeile][ord(Spalte)-65]=="x":
-    print("Treffer")
-else:
-    print("Kein Treffer")
+# print_grid()
+# Zeile=int(input("Welche Zeile"))-1
+# Spalte=(input("Welche Spalte"))
+#
+#
+# print_grid()
+# if board[Zeile][ord(Spalte)-65]=="x":
+#     print("Treffer")
+# else:
+#     print("Kein Treffer")
